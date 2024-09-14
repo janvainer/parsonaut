402d5520cd6480cbf36e4be773c55254d4506535
janvainer <vainerjan@gmail.com>
2024-09-14 17:59:37 +0200

    Vanilla working version

--- parsonaut/parse.py
+++ parsonaut/parse.py
@@ -1,7 +1,10 @@
-from argparse import Action
+import sys
+from argparse import SUPPRESS, Action
 from argparse import ArgumentParser as _ArgumentParser
 from argparse import ArgumentTypeError
+from types import SimpleNamespace
 
+from parsonaut.lazy import Choices, Lazy
 from parsonaut.typecheck import (
     Missing,
     get_flat_tuple_inner_type,
@@ -17,12 +20,43 @@ BOOL_FALSE_FLAGS = ("no", "false", "f", "n", "0")
 
 
 class ArgumentParser(_ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.lazy_roots = list()
+
+    def add_options(self, lzy: Lazy, dest: str):
+        assert "." not in dest
+        assert dest not in self.lazy_roots
+        self.lazy_roots.append(dest)
+        self._add_options(lzy, prefix=f"{dest}.")
+
+    def _add_options(self, lzy: Lazy, prefix: str = ""):
+        self.add_argument(f"--{prefix}_class", default=lzy.cls, help=SUPPRESS)
+        for k, (typ, value) in sorted(lzy.signature.items()):
+            if Lazy.is_lazy_type(typ):
+                if isinstance(value, Choices):
+                    if f"--{prefix}{k}" in sys.argv:
+                        val = sys.argv[sys.argv.index(f"--{prefix}{k}") + 1]
+                        value = type(value)[val]
+
+                    self.add_argument(
+                        f"--{prefix}{k}",
+                        type=str,
+                        choices=[e.name for e in type(value)],
+                        default=value.name,
+                    )
+                self._add_options(value, prefix=f"{prefix}{k}.")
+            else:
+                self.add_option(f"{prefix}{k}", value, typ)
+
     def add_option(self, name, value, typ):
         assert isinstance(name, str)
 
         name = f"--{name}"
         check_val = value if value is not Missing else None
         required = False
+
+        # bool
         if is_bool_type(typ):
             self.add_argument(
                 name,
@@ -32,6 +66,7 @@ class ArgumentParser(_ArgumentParser):
                 help="Parameter descripion.",
                 required=required,
             )
+        # int | float | str
         elif (
             is_int_type(typ, check_val)
             or is_str_type(typ, check_val)
@@ -45,6 +80,7 @@ class ArgumentParser(_ArgumentParser):
                 help="Parameter descripion.",
                 required=required,
             )
+        # tuple[bool | int | float |str , ...]
         elif is_flat_tuple_type(typ, check_val):
             subtyp, nitems = get_flat_tuple_inner_type(typ)
             nargs = "*" if nitems == -1 else nitems
@@ -65,6 +101,27 @@ class ArgumentParser(_ArgumentParser):
             )
         else:
             raise
+
+    def parse_args(self, args=None):
+        from collections import defaultdict
+
+        args = super().parse_args(args)
+        args_dict = vars(args)
+        args_grouped = defaultdict(dict)
+        for k, v in args_dict.items():
+            if not k.startswith(tuple(self.lazy_roots)):
+                args_grouped[k] = v
+            else:
+                root = [root for root in self.lazy_roots if k.startswith(root)]
+                assert len(root) == 1
+                root = root[0]
+                args_grouped[root][k.split(f"{root}.", 1)[-1]] = v
+
+        args_grouped = dict(args_grouped)
+        for root in self.lazy_roots:
+            args_grouped[root] = Lazy.from_dict(args_grouped[root])
+
+        return SimpleNamespace(**args_grouped)
 
 
 def collect_as(coll_type):
